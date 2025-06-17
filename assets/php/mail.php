6584705a2e3cb98b4e9930327938d1ea01b14123
preston-hrubes <phrubes@mutualofenumclaw.com>
2025-06-17 15:15:11 -0600

    Updated

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,5 +1,48 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+require __DIR__ . '/vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = htmlspecialchars($_POST['name'] ?? '');
+    $email = htmlspecialchars($_POST['email'] ?? '');
+    $message = htmlspecialchars($_POST['message'] ?? '');
+    $subject = htmlspecialchars($_POST["subject"] ?? '');
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; 
+        $mail->SMTPAuth = true;
+        $mail->Username = 'preston.m.hrubes@gmail.com'; 
+        $mail->Password = 'ostf trro drcx fodc'; 
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+
+        // Email contents
+        $mail->setFrom('preston.m.hrubes@gmail.com', 'Website Contact');
+        $mail->addAddress('preston.m.hrubes@gmail.com'); // Or wherever you want to receive emails
+        $mail->addReplyTo($email, $name);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'New message from your website';
+        $mail->Body    = "<strong>Name:</strong> $name<br><strong>Email:</strong> $email<br><strong>Subject:</strong>$subject<br><strong>Message:</strong><br>$message";
+        $mail->AltBody = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+        $mail->send();
+        echo "Message has been sent successfully.";
+    } catch (Exception $e) {
+        echo "Message could not be sent. Error: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request method.";
+}
 // Only process POST reqeusts.
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get the form fields and remove whitespace.
@@ -20,7 +63,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Set the recipient email address.
     // FIXME: Update this to your desired email address.
-    $recipient = "preston.hrubes@gmail.com";
+    $recipient = "preston.m.hrubes@gmail.com";
 
     // Set the email subject.
     $subject = "New contact from $name";
@@ -36,15 +79,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email_headers = "From: $name <$email>";
 
     // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
-        // Set a 200 (okay) response code.
-        http_response_code(200);
-        echo "Thank You! Your message has been sent.";
-    } else {
-        // Set a 500 (internal server error) response code.
-        http_response_code(500);
-        echo "Oops! Something went wrong and we couldn't send your message.";
-    }
+    // if (mail($recipient, $subject, $email_content, $email_headers)) {
+    //     // Set a 200 (okay) response code.
+    //     http_response_code(200);
+    //     echo "Thank You! Your message has been sent.";
+    // } else {
+    //     // Set a 500 (internal server error) response code.
+    //     http_response_code(500);
+    //     echo "Oops! Something went wrong and we couldn't send your message.";
+    // }
 } else {
     // Not a POST request, set a 403 (forbidden) response code.
     http_response_code(403);
---
+++ assets/php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'preston/php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '45e7f208a1ece15057ecf7b9a2d1791ff7c1f701',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.10.0',
+            'version' => '6.10.0.0',
+            'reference' => 'bf74d75a1fde6beaa34a0ddae2ec5fce0f72a144',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'preston/php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '45e7f208a1ece15057ecf7b9a2d1791ff7c1f701',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
